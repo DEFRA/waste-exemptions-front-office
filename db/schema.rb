af3445681dde65b76683bdbf8dfa83fbca7f24ef
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-10 22:28:26 +0000

    DB schema changes post adding location question

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181209222846) do
+ActiveRecord::Schema.define(version: 20181210170734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "enrollments", force: :cascade do |t|
+    t.string   "workflow_state"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "location"
+  end
+
+  create_table "waste_exemptions_engine_testies", force: :cascade do |t|
     t.string   "workflow_state"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
