8b439dc94c17a9e4fe2d8f82f32f675fce645e38
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-11 00:57:47 +0000

    Changes post adding applicant email page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181210232426) do
+ActiveRecord::Schema.define(version: 20181211003053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20181210232426) do
     t.string   "applicant_first_name"
     t.string   "applicant_last_name"
     t.string   "applicant_phone"
+    t.string   "applicant_email"
   end
 
 end
