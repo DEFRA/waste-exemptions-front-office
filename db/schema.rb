4d3e7982e7530bc7efe1933933246272f363849e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-11 00:28:55 +0000

    Changes post adding applicant phone page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181210223808) do
+ActiveRecord::Schema.define(version: 20181210232426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20181210223808) do
     t.string   "location"
     t.string   "applicant_first_name"
     t.string   "applicant_last_name"
+    t.string   "applicant_phone"
   end
 
 end
