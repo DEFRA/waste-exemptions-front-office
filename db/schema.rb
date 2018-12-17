14b7ca44451465284cd6d6f28d0ad00a2fc25ea1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-17 23:14:54 +0000

    DB schema changes after adding contact phone page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217223323) do
+ActiveRecord::Schema.define(version: 20181217230738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20181217223323) do
     t.string   "contact_first_name"
     t.string   "contact_last_name"
     t.string   "contact_position"
+    t.string   "contact_phone"
   end
 
   add_index "enrollments", ["token"], name: "index_enrollments_on_token", unique: true, using: :btree
