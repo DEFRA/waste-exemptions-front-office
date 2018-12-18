cab13754ada08c8ecbcdfc76b55cda6ab41aee1c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-12-18 00:01:53 +0000

    DB schema changes after adding is a farm page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217232226) do
+ActiveRecord::Schema.define(version: 20181217234231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20181217232226) do
     t.string   "contact_position"
     t.string   "contact_phone"
     t.string   "contact_email"
+    t.boolean  "is_a_farm"
   end
 
   add_index "enrollments", ["token"], name: "index_enrollments_on_token", unique: true, using: :btree
