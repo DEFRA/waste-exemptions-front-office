ac8e526592d610befc383d689acaf80e52dac54f
irisfaraway <iris.faraway@gmail.com>
2019-02-11 15:30:27 +0000

    Fix test mailer and add specs
    While working on https://github.com/DEFRA/waste-exemptions-back-office-ta/pull/30 I discovered that the way we store these config variables had changed but hadn't been updated. This resulted in errors in the mailer.
    
    This commit fixes those errors, and adds some tests to improve coverage and bring the TestMailer into line with its counterpart in the back office.

---
+++ spec/mailers/test_mailer_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TestMailer, type: :mailer do
+  describe "test_email" do
+    before do
+      allow(Rails.configuration).to receive(:email_test_address).and_return("test@example.com")
+      allow(WasteExemptionsEngine.configuration).to receive(:email_service_email).and_return("wex@example.com")
+      allow(WasteExemptionsEngine.configuration).to receive(:service_name).and_return("WEX")
+    end
+
+    let(:mail) { TestMailer.test_email }
+
+    it "uses the correct 'to' address" do
+      expect(mail.to).to eq(["test@example.com"])
+    end
+
+    it "uses the correct 'from' address" do
+      expect(mail.from).to eq(["wex@example.com"])
+    end
+
+    it "uses the correct subject" do
+      subject = "WEX email"
+      expect(mail.subject).to eq(subject)
+    end
+
+    it "includes the correct template in the body" do
+      expect(mail.body.encoded).to include("This is a test email")
+    end
+  end
+end
