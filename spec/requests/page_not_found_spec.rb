cd501232a529124093fb2f0eabd5ff977a31750d
cintamani <cintamani.puddu@gmail.com>
2019-04-26 15:42:26 +0100

    Fix rubocop issues

--- spec/requests/page_not_found_spec.rb
+++ spec/requests/page_not_found_spec.rb
@@ -7,7 +7,7 @@ module WasteExemptionsEngine
     describe "GET /this-page-does-not-exist" do
       it "redirects to `/errors/404`" do
         rails_respond_without_detailed_exceptions do
-          get '/this-page-does-not-exist'
+          get "/this-page-does-not-exist"
 
           expect(response.code).to eq("404")
         end
