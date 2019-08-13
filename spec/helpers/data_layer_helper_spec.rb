d7fb8f463f9551873b25730ffdc59401e72f3bdf
irisfaraway <iris.faraway@gmail.com>
2019-08-13 15:29:05 +0100

    Add DataLayerHelper
    This is used to output a formatted string which will be included in the data layer in the layout. The format should be `'journey': 'new'` but the code allows for us to add more pairs to the hash in the future.

---
+++ spec/helpers/data_layer_helper_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe DataLayerHelper, type: :helper do
+  describe "data_layer" do
+    context "when the transient_registration is an EditRegistration" do
+      let(:transient_registration) { build(:edit_registration) }
+
+      it "returns the correct value" do
+        expected_string = "'journey': 'edit'"
+
+        expect(helper.data_layer(transient_registration)).to eq(expected_string)
+      end
+    end
+
+    context "when the transient_registration is a NewRegistration" do
+      let(:transient_registration) { build(:new_registration) }
+
+      it "returns the correct value" do
+        expected_string = "'journey': 'new'"
+
+        expect(helper.data_layer(transient_registration)).to eq(expected_string)
+      end
+    end
+
+    context "when the transient_registration is a RenewingRegistration" do
+      let(:transient_registration) { build(:renewing_registration) }
+
+      it "returns the correct value" do
+        expected_string = "'journey': 'renew'"
+
+        expect(helper.data_layer(transient_registration)).to eq(expected_string)
+      end
+    end
+  end
+end
