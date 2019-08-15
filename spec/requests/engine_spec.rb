5b5b6a28ef7f04bf80c035e2c115ce10dd2e2e10
Iris Faraway <iris.faraway@gmail.com>
2019-08-15 10:52:34 +0100

    Test that engine is mounted correctly (#197)
    This conveniently bumps our test coverage up to 100%. What a coincidence!

---
+++ spec/requests/engine_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Waste Exemptions Engine", type: :request do
+  describe "/start/new" do
+    let(:request_path) { "/start/new" }
+
+    it "returns a 200 response" do
+      get request_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
