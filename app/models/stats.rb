a12fa106c2892a152c29aba4da538f0dfc471fa4
Stuart Adair <stu@silverka.co.uk>
2019-09-10 17:00:25 +0100

    Renamed Stats private methods

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -4,8 +4,22 @@ class Stats
   attr_reader :new_reg, :assisted_pc, :email_renewals
 
   def initialize
-    @new_reg = 25
-    @assisted_pc = 35.4
-    @email_renewals = 17
+    @new_reg = calculate_new_reg
+    @assisted_pc = calculate_assisted_pc
+    @email_renewals = calculate_email_renewals
+  end
+
+  private
+
+  def calculate_new_reg
+    25
+  end
+
+  def calculate_assisted_pc
+    35.4
+  end
+
+  def calculate_email_renewals
+    17
   end
 end
