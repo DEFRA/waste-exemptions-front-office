9d72db154da10033e4bc9d52de86ce205c1593a3
Stuart Adair <stu@silverka.co.uk>
2019-09-10 15:00:34 +0100

    Fix rubocop offences

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,14 +2,16 @@
 
 class StatsController < ActionController::Base
 
-# new_reg = The total number of new registrations (not including email renewals) in the past week.
-# assisted_pc = The ratio of (fully assisted and partially assisted) to (total of new registrations and renewals) as a percentage to one decimal place.
-# email_renewals = The total number of renewals by email in the past week.
+  # new_reg = The total number of new registrations (not including email renewals) in the past week.
+
+  # assisted_pc = The ratio of (fully assisted and partially assisted) to (total of new registrations and renewals)
+  # as a percentage to one decimal place.
+
+  # email_renewals = The total number of renewals by email in the past week.
 
   protect_from_forgery with: :exception
 
   def index
-
     stats = Stats.new
 
     render json: {
