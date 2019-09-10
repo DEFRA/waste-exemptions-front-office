996bfae3c3bf33c4b75945763b5615f38d6c1226
Stuart Adair <stu@silverka.co.uk>
2019-09-10 12:22:52 +0100

    Fix rubocop errors

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,10 +3,10 @@
 class StatsController < ActionController::Base
 
   def index
-    render :json => {
-      :new_reg => 25,
-      :assisted_pc => 35.4,
-      :email_renewals => 17
+    render json: {
+      new_reg: 25,
+      assisted_pc: 35.4,
+      email_renewals: 17
     }
   end
 
