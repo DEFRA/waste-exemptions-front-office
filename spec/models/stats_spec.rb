0fe909c82b9722779ef507da1a8141efd19add78
Stuart Adair <stu@silverka.co.uk>
2019-09-11 16:12:06 +0100

    Tidy date range test

--- spec/models/stats_spec.rb
+++ spec/models/stats_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe Stats, type: :model do
   subject { described_class.new }
 
   describe "#new_reg" do
-    it "only counts registrations in the correct range" do
-      create(:registration, submitted_at: Date.today)
-      create(:registration, submitted_at: Date.today - 1)
-      create(:registration, submitted_at: Date.today - 7)
-      create(:registration, submitted_at: Date.today - 8)
-      expect(subject.new_reg).to eq(2)
+    it "only counts registrations in the correct date range" do
+      (0..8).each do |day|
+        create(:registration, submitted_at: Date.today - day)
+      end
+      expect(subject.new_reg).to eq(7)
     end
 
     it "only counts new registrations" do
