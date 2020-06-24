13cdac2b67f332b9f5e5d7e45528f9096082a5ab
cintamani <cintamani.puddu@gmail.com>
2020-06-24 09:50:42 +0100

    Disable enum functionality on factory bot 6 (#360)
    Check: https://github.com/thoughtbot/factory_bot_rails/issues/384 for more info

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,8 @@
 # Require this here to get factories to play nice with engine
 require "factory_bot_rails"
 
+FactoryBot.automatically_define_enum_traits = false
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
