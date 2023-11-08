cd13b5f18ff0e6d3550176b046544dcd92427531
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-11-08 09:47:45 +0000

    Update gems and fix lint issues

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe "Cookies" do
     visit root_path
     expect(page).to have_link("View cookies", href: "/pages/cookies")
 
-    click_on "Accept analytics cookies"
+    click_button "Accept analytics cookies"
     expect(page).to have_text("You’ve accepted analytics cookies")
 
     within cookie_banner_div do
       expect(page).to have_link("change your cookie settings", href: "/cookies/edit")
-      click_on "Hide this message"
+      click_button "Hide this message"
     end
 
     expect(page).not_to have_css(cookie_banner_div)
@@ -28,20 +28,20 @@ RSpec.describe "Cookies" do
 
   it "User rejects analytics cookies and toggles their selection" do
     visit root_path
-    click_on "Reject analytics cookies"
+    click_button "Reject analytics cookies"
     expect(page).to have_text("You’ve rejected analytics cookies")
     expect(page.source).not_to have_text(google_analytics_render_tag)
 
-    click_on "change your cookie settings"
+    click_link "change your cookie settings"
     expect(page).to have_css("h1", text: "Cookies on Register your waste exemptions")
 
     choose "Use cookies that measure my website use"
-    click_on "Save and continue"
+    click_button "Save and continue"
     expect(page.source).to have_text(google_analytics_render_tag)
     expect(page).to have_text("You’ve set your cookie preferences.")
 
     choose "Do not use cookies that measure my website use"
-    click_on "Save and continue"
+    click_button "Save and continue"
     expect(page.source).not_to have_text(google_analytics_render_tag)
   end
 end
