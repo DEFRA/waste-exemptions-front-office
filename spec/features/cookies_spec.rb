7245410ff25c89f3e3e565f0ecefbedbfde52513
brujeo <brujev@gmail.com>
2024-01-09 15:23:41 +0000

    update dependencies (#845)

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -14,35 +14,35 @@ RSpec.describe "Cookies" do
     visit root_path
     expect(page).to have_link("View cookies", href: "/pages/cookies")
 
-    click_button "Accept analytics cookies"
+    click_on "Accept analytics cookies"
     expect(page).to have_text("You’ve accepted analytics cookies")
 
     within cookie_banner_div do
       expect(page).to have_link("change your cookie settings", href: "/cookies/edit")
-      click_button "Hide this message"
+      click_on "Hide this message"
     end
 
-    expect(page).not_to have_css(cookie_banner_div)
+    expect(page).to have_no_css(cookie_banner_div)
     expect(page.source).to have_text(google_analytics_render_tag)
   end
 
   it "User rejects analytics cookies and toggles their selection" do
     visit root_path
-    click_button "Reject analytics cookies"
+    click_on "Reject analytics cookies"
     expect(page).to have_text("You’ve rejected analytics cookies")
-    expect(page.source).not_to have_text(google_analytics_render_tag)
+    expect(page.source).to have_no_text(google_analytics_render_tag)
 
-    click_link "change your cookie settings"
+    click_on "change your cookie settings"
     expect(page).to have_css("h1", text: "Cookies on Register your waste exemptions")
 
     choose "Use cookies that measure my website use"
-    click_button "Save and continue"
+    click_on "Save and continue"
     expect(page.source).to have_text(google_analytics_render_tag)
     expect(page).to have_text("You’ve set your cookie preferences.")
 
     choose "Do not use cookies that measure my website use"
-    click_button "Save and continue"
-    expect(page.source).not_to have_text(google_analytics_render_tag)
+    click_on "Save and continue"
+    expect(page.source).to have_no_text(google_analytics_render_tag)
   end
 end
 # rubocop:enable RSpec/MultipleExpectations
