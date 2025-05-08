1976bbf1fabf408a6141b4c024d762871783c3ee
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-05-08 15:47:34 +0100

    Add defra_ruby_mocks

---
+++ config/initializers/defra_ruby_aws.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "defra_ruby/aws"
+
+DefraRuby::Aws.configure do |c|
+  govpay_mocks_bucket = {
+    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET"),
+    region: ENV.fetch("AWS_REGION", "eu-west-1"),
+    credentials: {
+      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID"),
+      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY")
+    },
+    encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", false)
+  }
+
+  c.buckets = [govpay_mocks_bucket]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   mount WasteExemptionsEngine::Engine => "/"
 
+  mount DefraRubyMocks::Engine => "/fo/mocks"
+
   root "waste_exemptions_engine/start_forms#new"
 
   get "/Getting_ready_for_change", to: "pages#getting_ready_for_change"
