e4ca23d9f7fe1d59a919a86ee440ae4fa17e0af2
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-05-19 18:39:48 +0100

    Fix mock env var names

--- config/initializers/defra_ruby_mocks.rb
+++ config/initializers/defra_ruby_mocks.rb
@@ -3,11 +3,11 @@
 DefraRubyMocks.configure do |configuration|
   # Enable the mock routes mounted in this app if the environment is configured
   # for it
-  configuration.enable = ENV["WCRS_MOCK_ENABLED"] || false
+  configuration.enable = ENV["WEX_MOCK_ENABLED"] || false
   # Set how long the mock should delay before responding. In the engine itself
   # the default is 1000ms (1 second)
-  configuration.delay = ENV["WCRS_MOCK_DELAY"] || 1000
+  configuration.delay = ENV["WEX_MOCK_DELAY"] || 1000
 
   # Govpay API mock details. Note FO application point to BO mocks and vice-versa by defafult.
-  configuration.govpay_domain = ENV["WCRS_MOCK_FO_GOVPAY_URL"] || "http://localhost:8001/bo/mocks/govpay/v1"
+  configuration.govpay_domain = ENV["WEX_MOCK_FO_GOVPAY_URL"] || "http://localhost:8000/bo/mocks/govpay/v1"
 end
