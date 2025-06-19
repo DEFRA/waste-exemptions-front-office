981052d6e27e95318db7be3eb55a03fbc528931f
Paul Doyle <97455399+PaulDoyle-EA@users.noreply.github.com>
2025-06-19 17:10:32 +0100

    Update govpay mocks config (#1232)
    https://eaflood.atlassian.net/browse/RUBY-3887

--- config/initializers/defra_ruby_mocks.rb
+++ config/initializers/defra_ruby_mocks.rb
@@ -8,11 +8,12 @@ DefraRubyMocks.configure do |configuration|
   # the default is 1000ms (1 second)
   configuration.delay = ENV["WEX_MOCK_DELAY"] || 1000
 
-  # Govpay API mock details. Note FO application point to BO mocks and vice-versa by defafult.
+  # Govpay API mock details. Note FO application point to BO mocks and vice-versa by default
+  # so they don't block in a single-process application (e.g. local vagrant).
   configuration.govpay_mocks_external_root_url = ENV.fetch("WEX_MOCK_FO_GOVPAY_URL", "http://localhost:8000/bo/mocks/govpay/v1")
   configuration.govpay_mocks_external_root_url_other = ENV.fetch("WEX_MOCK_BO_GOVPAY_URL", "http://localhost:3000/fo/mocks/govpay/v1")
 
-  # On our hosted environments, the FO/BOmocks are accessible to each other via different URLs:
+  # On our hosted environments, the FO/BO mocks are accessible to each other via different URLs:
   configuration.govpay_mocks_internal_root_url = ENV.fetch("WEX_MOCK_FO_GOVPAY_URL_INTERNAL", "http://localhost:8000/bo/mocks/govpay/v1")
   configuration.govpay_mocks_internal_root_url_other = ENV.fetch("WEX_MOCK_BO_GOVPAY_URL_INTERNAL", "http://localhost:3000/fo/mocks/govpay/v1")
 end
