f13439734a55503415cefd0b6293824d48d24e81
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-06-19 16:57:11 +0100

    Add defaults for mocks env var lookups

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -4,11 +4,11 @@ require "defra_ruby/aws"
 
 DefraRuby::Aws.configure do |c|
   govpay_mocks_bucket = {
-    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET"),
+    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
     region: ENV.fetch("AWS_REGION", "eu-west-1"),
     credentials: {
-      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID"),
-      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY")
+      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", nil),
+      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", nil)
     },
     encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", false)
   }
