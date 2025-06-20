31b9b1bdfd94cd988a6c6495fd2cf3cb8a2a0733
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-06-20 12:57:15 +0100

    Skip mocks s3 bucket config if bucket name not defined

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -6,17 +6,17 @@ DefraRuby::Aws.configure do |c|
   c.buckets = []
 
   mocks_bucket_name = ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil)
-  return unless mocks_bucket_name.present?
-
-  govpay_mocks_bucket = {
-    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
-    region: ENV.fetch("AWS_REGION", "eu-west-1"),
-    credentials: {
-      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", nil),
-      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", nil)
-    },
-    encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", false)
-  }
-
-  c.buckets << govpay_mocks_bucket
+  if mocks_bucket_name.present?
+    govpay_mocks_bucket = {
+      name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
+      region: ENV.fetch("AWS_REGION", "eu-west-1"),
+      credentials: {
+        access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", nil),
+        secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", nil)
+      },
+      encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", false)
+    }
+  
+    c.buckets << govpay_mocks_bucket
+  end
 end
