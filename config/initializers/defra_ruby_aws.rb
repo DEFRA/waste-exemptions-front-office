0afb1e284ccfb6f7a39b4b9d367adcaa0297af62
Paul Doyle <97455399+PaulDoyle-EA@users.noreply.github.com>
2025-06-23 13:25:38 +0100

    Chore/govpay mocks setup (#1237)
    https://eaflood.atlassian.net/browse/RUBY-3887

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -3,14 +3,15 @@
 require "defra_ruby/aws"
 
 DefraRuby::Aws.configure do |c|
+
   govpay_mocks_bucket = {
-    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", nil),
+    name: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_BUCKET", "dummy"),
     region: ENV.fetch("AWS_REGION", "eu-west-1"),
     credentials: {
-      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", nil),
-      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", nil)
+      access_key_id: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ACCESS_KEY_ID", "1"),
+      secret_access_key: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_SECRET_ACCESS_KEY", "1")
     },
-    encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", false)
+    encrypt_with_kms: ENV.fetch("AWS_DEFRA_RUBY_MOCKS_ENCRYPT_WITH_KMS", "1")
   }
 
   c.buckets = [govpay_mocks_bucket]
