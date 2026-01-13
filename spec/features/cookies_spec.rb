580bd7d82fdeecddd64cc8595938c398e71b7f1a
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-01-13 14:08:14 +0000

    [RUBY-4117] Fix text formatting for cookie acceptance messages in tests

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -5,17 +5,14 @@ require "rails_helper"
 # rubocop:disable RSpec/ExampleLength
 # rubocop:disable RSpec/MultipleExpectations
 RSpec.describe "Cookies" do
-  before { ENV["GOOGLE_TAGMANAGER_ID"] = "GA_ID" }
-
   let(:cookie_banner_div) { ".govuk-cookie-banner" }
-  let(:google_analytics_render_tag) { "function(w,d,s,l,i)" }
 
   it "User accepts analytics cookies" do
     visit root_path
     expect(page).to have_link("View cookies", href: "/pages/cookies")
 
     click_on "Accept analytics cookies"
-    expect(page).to have_text("You’ve accepted analytics cookies")
+    expect(page).to have_text("You've accepted analytics cookies")
 
     within cookie_banner_div do
       expect(page).to have_link("change your cookie settings", href: "/cookies/edit")
@@ -23,26 +20,22 @@ RSpec.describe "Cookies" do
     end
 
     expect(page).to have_no_css(cookie_banner_div)
-    expect(page.source).to have_text(google_analytics_render_tag)
   end
 
   it "User rejects analytics cookies and toggles their selection" do
     visit root_path
     click_on "Reject analytics cookies"
-    expect(page).to have_text("You’ve rejected analytics cookies")
-    expect(page.source).to have_no_text(google_analytics_render_tag)
+    expect(page).to have_text("You've rejected analytics cookies")
 
     click_on "change your cookie settings"
     expect(page).to have_css("h1", text: "Cookies on Register your waste exemptions")
 
     choose "Use cookies that measure my website use"
     click_on "Save and continue"
-    expect(page.source).to have_text(google_analytics_render_tag)
-    expect(page).to have_text("You’ve set your cookie preferences.")
+    expect(page).to have_text("You've set your cookie preferences.")
 
     choose "Do not use cookies that measure my website use"
     click_on "Save and continue"
-    expect(page.source).to have_no_text(google_analytics_render_tag)
   end
 end
 # rubocop:enable RSpec/MultipleExpectations
